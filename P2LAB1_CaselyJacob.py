a0899fc04a00ad110f9ec1e41e368eaff19d5d89
caselyj1413 <124919801+caselyj1413@users.noreply.github.com>
2023-02-21 13:40:36 -0500

    Add files via upload
    Driving Costs

---
+++ P2LAB1_CaselyJacob.py
@@ -0,0 +1,9 @@
+''' Type your code here. '''
+mpg = float(input())
+cpg = float(input())
+cost_20 = 20 / mpg * cpg
+cost_75 = 75 / mpg * cpg
+cost_500 = 500 / mpg * cpg
+
+
+print(f'{cost_20:.2f} {cost_75:.2f} {cost_500:.2f}')
